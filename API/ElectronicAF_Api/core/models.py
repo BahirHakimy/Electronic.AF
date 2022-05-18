752e35c9ead75742a5d750a31d28b19f74bf3ddd
Bahir Hakimy <bahirhakimy2015@gmail.com>
2022-05-18 12:36:48 +0430

    Authentication Development completed

--- API/ElectronicAF_Api/core/models.py
+++ API/ElectronicAF_Api/core/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.base_user import AbstractBaseUser,BaseUserManager
+from django.contrib.auth.models import AbstractUser,UserManager,PermissionsMixin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.utils import timezone
+from django.core.mail import send_mail
 
-# Create your models here.
+class CustomManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email,password,**extra_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(verbose_name='email address',unique=True)
+    phone = models.CharField(max_length=9)
+    firstname = models.CharField(('first name'), max_length=150)
+    lastname = models.CharField(('last name'), max_length=150)
+    is_staff = models.BooleanField(
+        ('staff status'),
+        default=False,
+        help_text=('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        ('active'),
+        default=True,
+        help_text=(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(('date joined'), default=timezone.now)
+
+    objects= CustomManager()
+
+    class Meta:
+        verbose_name = ("user")
+        verbose_name_plural = ("users")
+
+    def __str__(self) -> str:
+        return self.email
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        fullname = '%s %s' % (self.firstname, self.lastname)
+        return fullname.strip()
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
