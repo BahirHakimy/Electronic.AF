c3df18ab8668f32f2722ff008eaca4afd9914d31
M@W <mehdicr7wahid@gmail.com>
2024-06-01 21:11:38 +0430

    modified radix drop down menu

--- Frontend/src/components/menudropdown.jsx
+++ Frontend/src/components/menudropdown.jsx
@@ -1,19 +1,48 @@
 import * as DropDownMenu from "@radix-ui/react-dropdown-menu";
 import React from "react";
+import { Link } from "react-router-dom";
+import { SiDell, SiHp, SiLenovo } from "react-icons/si";
 
 function Menu() {
   return (
     <DropDownMenu.Root>
-      <DropDownMenu.Trigger className="text-xl">
+      <DropDownMenu.Trigger className="text-[22px] hover:scale-110 transition-transform duration-300">
         Categories
       </DropDownMenu.Trigger>
-      <DropDownMenu.Portal className="bg-white">
-        <DropDownMenu.Content>
-          <DropDownMenu.Item className="px-4 py-2 hover:bg-cyan-400">
-            HP
-          </DropDownMenu.Item>
-          <DropDownMenu.Item>Lenovo</DropDownMenu.Item>
-          <DropDownMenu.Item>Dell</DropDownMenu.Item>
+      <DropDownMenu.Portal>
+        <DropDownMenu.Content
+          sideOffset={5}
+          className="bg-white rounded-lg min-w-32 text-xl shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade py-4 px-2  "
+        >
+          <DropDownMenu.Group className="space-y-2">
+            <DropDownMenu.Item asChild>
+              <Link
+                to={""}
+                className="flex items-center gap-2 hover:bg-primaryLight py-1 rounded"
+              >
+                <SiHp />
+                HP
+              </Link>
+            </DropDownMenu.Item>
+
+            <DropDownMenu.Item asChild>
+              <Link className="flex items-center gap-2 rounded hover:bg-primaryLight cursor-pointer py-1">
+                <SiLenovo />
+                Lenovo
+              </Link>
+            </DropDownMenu.Item>
+
+            <DropDownMenu.Item asChild>
+              <Link
+                to={""}
+                className="flex items-center gap-2 rounded hover:bg-primaryLight cursor-pointer py-1"
+              >
+                <SiDell />
+                Dell
+              </Link>
+            </DropDownMenu.Item>
+          </DropDownMenu.Group>
+          <DropDownMenu.Arrow className="fill-white" />
         </DropDownMenu.Content>
       </DropDownMenu.Portal>
     </DropDownMenu.Root>
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         background: "#F0F0F0",
@@ -25,6 +18,33 @@ module.exports = {
       fontFamily: {
         roboto: ["Roboto"],
         robotoBold: ["Roboto Bold"],
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: "0", transform: "translateY(-2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideLeftAndFade: {
+          from: { opacity: "0", transform: "translateX(2px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        slideUpAndFade: {
+          from: { opacity: "0", transform: "translateY(2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideRightAndFade: {
+          from: { opacity: "0", transform: "translateX(-2px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
